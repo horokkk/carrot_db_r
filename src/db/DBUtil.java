f9caae19ba6232df34859f61dd139ecb300f8b18
horokkk <smurfystrawberry@gmail.com>
2025-06-06 04:32:32 +0900

    jdbc-sql 연동 설명 추가가

--- src/db/DBUtil.java
+++ src/db/DBUtil.java
@@ -3,7 +3,7 @@ package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class DBUtil {
+public class DBUtil { //사용자에 맞게 URL, USER, PASSWORD 수정할 것
     private static final String URL = "jdbc:mysql://localhost:3306/carrot_db?useUnicode=true&characterEncoding=UTF-8"; // ✅ 로컬 DB 주소
     private static final String USER = "root";     // ✅ 로컬 MySQL 사용자명
     private static final String PASSWORD = "0118"; // ✅ 로컬 MySQL 비번
